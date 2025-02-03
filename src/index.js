2d1512b67bae7956c3c61a1d12c1ec0ec68a834b
Robert Washko <robert.washko@gmail.com>
2025-02-03 18:20:25 -0500

    updated main app import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MainApp from './MainApp';  // ✅ Import MainApp correctly
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
