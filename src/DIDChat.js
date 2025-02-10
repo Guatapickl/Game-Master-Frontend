dbbe9cb60f0df7011ca77935e5bfebd6c74fc111
Robert Washko <robert.washko@gmail.com>
2025-02-10 18:40:44 -0500

    gradient

--- src/DIDChat.js
+++ src/DIDChat.js
@@ -120,7 +120,8 @@ function DIDChat() {
         textAlign: 'center',
         textShadow: '0 0 10px #00FFFF, 0 0 20px #00FFFF',
         letterSpacing: '2px',
-        marginBottom: '10px'
+        marginBottom: '10px',
+        background: 'linear-gradient(90deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4))',
       }}>
         <span style={{ textDecoration: 'underline' }}>S</span>ubspace <span style={{ textDecoration: 'underline' }}>L</span>iaison <span style={{ textDecoration: 'underline' }}>U</span>niversal <span style={{ textDecoration: 'underline' }}>T</span>ransceiver
       </h1>
